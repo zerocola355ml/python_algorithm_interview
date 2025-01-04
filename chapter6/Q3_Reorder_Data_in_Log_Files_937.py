2e4dcc2b5a2376cd041fa61068db07e6273eafe0
신헌주 <sin51611630@gmail.com>
2025-01-04 22:11:03 +0900

    add leetcode937 solution

---
+++ chapter6/Q3_Reorder_Data_in_Log_Files_937.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        letters = []
+        digits = []
+        for log in logs:
+            if log.split()[1].isnumeric():
+                digits.append(log)
+            else:
+                letters.append(log)
+
+        letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+
+        return letters + digits
